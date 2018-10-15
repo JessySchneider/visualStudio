1b1b0f595aa3812daa1a06e8d0b0dc4512b150a2
Jessy SCHNEIDER <schneider.jessy54@gmail.com>
2018-10-15 14:04:17 +0200

    Test de mise en ligne

--- js/script.js
+++ js/script.js
@@ -3,4 +3,22 @@ Number.prototype.carre = function(){
     return this*this;
 }
 
-console.log("Hello world");
+String.prototype.checkAndConcat = function (a, ...args){
+
+    if( a === String(this)){
+        var message = a;
+        for(var i=0; i<args.length;i++){
+            message = message + " "+args[i];
+        }
+        console.log(message);
+        return message;
+    }else{
+        var message = "Erreur, le premier mot n'est pas identique";
+        console.log(message);
+        return message;
+    }
+    
+}
+
+"Salut".checkAndConcat("Salut","toto","tata","tutu","blabla","heyhey");
+"Hello".checkAndConcat("Salut","toto","tata","tutu","blabla","heyhey");
