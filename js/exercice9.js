6a110f01ac089c73e959c61cfaa0dc4794b444de
Jessy SCHNEIDER <schneider.jessy54@gmail.com>
2018-10-17 08:42:06 +0200

    Exercice fonction array map

--- js/exercice9.js
+++ js/exercice9.js
@@ -4,14 +4,26 @@ Array.prototype.customMap = function(func){
 
     var newTab = new Array();
     this.forEach(function(value,index){
-        var myFunction = func;
-        newTab[index] = String(value).myFunction();
+        newTab[index] = func(value);
     });
-    console.log(newTab);
     return newTab;
-    
 }
 
 var tab = ["a","b",'c'];
-var tableauTraite = tab.customMap("toUpperCase");
+var tabNb = [2,4,6,8,10];
+var tableauTraite = tab.customMap(miseEnMajuscule);
+var tableauTraiteNb = tabNb.customMap(carre);
 console.log(tableauTraite);
+console.log(tableauTraiteNb);
+console.log(tab);
+console.log(tabNb);
+var compTab = tabNb.map(carre);
+console.log(compTab);
+
+function miseEnMajuscule(string){
+    return string.toUpperCase();
+}
+
+function carre(number){
+    return number*number;
+}
